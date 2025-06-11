19e6a01c0b706c5917f29685bdc2db36b42fe2c9
undefined undefined undefined <85342986+Nikhil-Lathigara@users.noreply.github.com>
2025-06-11 16:05:47 +0530

    Delete backend directory

--- backend/config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb+srv://nikhilsoni1209:ht9LUB8UAhximYYz@cluster0.n1r3h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-    console.log("MongoDB connected successfully!");
-})
-.catch((err) => {
-    console.error("MongoDB connection error:", err.message);
-});
